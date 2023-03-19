ba15bfa1451c7be9f4c9d9160a2633693e82c1d5
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-03-19 14:42:55 +0530

    Alpha (#2)
    * Coomit First
    
    * Comit second Uis

--- app/src/main/java/com/example/uimad/MainActivity.kt
+++ app/src/main/java/com/example/uimad/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.uimad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.setuppage4)
+        setContentView(R.layout.page15)
+
+//                getWindow().setFlags(
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
